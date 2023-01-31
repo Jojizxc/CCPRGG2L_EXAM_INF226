6eb9eaae15a8c234052ed4e3989a8de4d870f1b9
Bianca Louise Rosales <119952034+Jojizxc@users.noreply.github.com>
2023-01-31 03:43:42 -1200

    Add files via upload

---
+++ EXAM #1/Recursion.java
@@ -0,0 +1,62 @@
+public class Recursion {
+
+    public static void main(String[] args) {
+        
+        // Remove letters (30 pts)
+
+        // Instructions (Important!)
+
+        // Without using arrays or loops or regular expressions, apply recursion to remove specific letters in
+        // your full name.
+
+        // 1. If the last letter of your surname is a vowel, remove all vowels in
+        // your full name.
+
+        // 2. If the last letter of your surname is a consonant, remove all
+        // consonants in your full name.
+
+        // 3. Use only lowercase letters
+        
+        // 4. Each letter should be removed one by one.
+
+        // Example:
+        // Full name is "elizer ponio jr".
+        // The output should be "lzr pn jr".
+
+        // Put your complete name in the "fullName" variable.
+        // Example: 
+        // String fullName = "elizer ponio jr";
+
+        String fullName = "bianca louise rosales";
+        System.out.println("ENTER NAME:");
+
+        removeLetter(fullName);
+    }
+
+    static void removeLetter(String str) {  
+ 
+        if (str.length()==0){
+            return;
+        }
+
+    
+        char vowel = str.charAt(0);
+        if (vowel == 'a' | vowel == 'e' | vowel == 'i' | vowel == 'o' | vowel == 'u'| vowel == ' '){
+            System.out.print(vowel);
+
+        }
+
+        removeLetter(str.substring(1)); 
+
+        
+    
+
+         }   
+        
+        }
+
+
+
+
+        
+    
