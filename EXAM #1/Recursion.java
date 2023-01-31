f9674af38970f95c348ba6558547d3c2636688fc
Bianca Louise Rosales <119952034+Jojizxc@users.noreply.github.com>
2023-01-31 03:53:52 -1200

    Update Recursion.java

--- EXAM #1/Recursion.java
+++ EXAM #1/Recursion.java
@@ -53,9 +53,33 @@ public class Recursion {
 
          }   
         
+        } 
+//CONSONANT RESULT 
+
+// Scanner sc =new Scanner(System.in);
+
+
+
+     //   String str1,str2="";
+       // int i;
+
+        //System.out.println("ENTER NAME:\n");
+        //str1=sc.nextLine(); 
+
+        //for(i=0;i<str1.length();i++) {
+
+           // if (str1.charAt(i)=='A'||str1.charAt(i)=='E'||str1.charAt(i)=='I'||str1.charAt(i)=='O'||str1.charAt(i)=='U'||str1.charAt(i)=='a'||
+            //str1.charAt(i)=='e'||str1.charAt(i)=='i'||str1.charAt(i)=='o'||str1.charAt(i)=='u');
+       //else 
+       
+       {
+
+        //str2=str2+str1.charAt(i);
+
+       }
+
         }
-
-
+        //System.out.println(str2); 
 
 
         
